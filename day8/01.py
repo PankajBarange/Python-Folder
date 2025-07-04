87bca40723c2624e24484afe50aa1c6180507ea7
Pankaj <johndoe@example.com>
2025-07-04 09:35:11 +0530

    class

---
+++ day8/01.py
@@ -0,0 +1,34 @@
+class bank:
+    BankName = "HDFC Bank"
+
+    def getStatus(self):
+        print(f"The Bank Name = {self.BankName}")
+        print(f"The Acc Balancr = {self.acc_balance}")
+        print("==================================\n")
+
+    def deposit(self,amount):
+        self.acc_balance = self.acc_balance + amount    
+        print("Your amount has been credited!")
+        print("================================\n")
+
+
+    def withdraw(self, amount):
+        if self.acc_balance > amount:
+            self.acc_balance = self.acc_balance - amount
+            print("Your amound has been debited")
+            print("=================================\n")
+        else:
+            print("Insufficient Amount")
+
+
+
+b1 = bank()
+b1.acc_balance = 500
+b1.getStatus()
+
+b1.deposit(300)
+b1.getStatus()
+
+
+b1.withdraw(500)
+b1.getStatus()
